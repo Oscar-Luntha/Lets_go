333ac33a734c730e14ccff9a8135b2c3664c1f8a
Oscar Luntha <lunthaoscar@gmail.com>
2026-08-25 15:24:18 +0200

    Refactor main function to set up HTTP server and route handling

--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello, welcome to the home page"))
 }
 func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
 
+	fmt.Println("Server listening on port 4000")
+	err := http.ListenAndServe(":4000", mux)
+
+	log.Fatal(err)
 }
