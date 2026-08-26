097f4e10df73d3404a895ed48575011326849088
Oscar Luntha <lunthaoscar@gmail.com>
2026-08-26 22:16:30 +0200

    Refactor main function to use application struct for handler methods

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -18,16 +18,19 @@ func main() {
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
+	app := &application{
+		logger: logger,
+	}
 	mux := http.NewServeMux()
 	// create file server for serving static files
 	fileServer := http.FileServer(http.Dir("./ui/static"))
 	// register the file server
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
-	mux.HandleFunc("GET /{$}", home)
-	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
-	mux.HandleFunc("GET /snippet/create", getSnippetCreate)
-	mux.HandleFunc("POST /snippet/create", postSnippetCreate)
+	mux.HandleFunc("GET /{$}", app.home)
+	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
+	mux.HandleFunc("GET /snippet/create", app.getSnippetCreate)
+	mux.HandleFunc("POST /snippet/create", app.postSnippetCreate)
 
 	logger.Info("Starting server", "addr", *addr)
 	err := http.ListenAndServe(*addr, mux)
