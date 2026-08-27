c0771285121da9d5385530d4731e8054263d5b4a
Oscar Luntha <lunthaoscar@gmail.com>
2026-08-27 14:12:31 +0200

    Refactor main function to use app.routes() for handling HTTP requests

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -15,26 +15,12 @@ func main() {
 
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
-
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
-
 	app := &application{
 		logger: logger,
 	}
-	mux := http.NewServeMux()
-	// create file server for serving static files
-	fileServer := http.FileServer(http.Dir("./ui/static"))
-	// register the file server
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
-	mux.HandleFunc("GET /snippet/create", app.getSnippetCreate)
-	mux.HandleFunc("POST /snippet/create", app.postSnippetCreate)
-
 	logger.Info("Starting server", "addr", *addr)
-	err := http.ListenAndServe(*addr, mux)
-
+	err := http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
 }
