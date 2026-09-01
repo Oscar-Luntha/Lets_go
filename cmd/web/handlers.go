25e7f7b1a4f987e5034e5c802dbf8bf52ce81342
Oscar Luntha <lunthaoscar@gmail.com>
2026-09-01 22:11:55 +0200

    Refactor template rendering in home and snippet view handlers to use newTemplateData function for improved data management

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -16,10 +16,9 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		app.serverError(w, r, err)
 		return
 	}
-
-	app.render(w, r, http.StatusOK, "home.tmpl.html", templateData{
-		Snippets: snippets,
-	})
+	data := app.newTemplateData(r)
+	data.Snippets = snippets
+	app.render(w, r, http.StatusOK, "home.tmpl.html", data)
 }
 
 func (app *application) getSnippetCreate(w http.ResponseWriter, r *http.Request) {
@@ -55,7 +54,7 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
-	app.render(w, r, http.StatusOK, "view.tmpl.html", templateData{
-		Snippet: snippet,
-	})
+	data := app.newTemplateData(r)
+	data.Snippet = snippet
+	app.render(w, r, http.StatusOK, "view.tmpl.html", data)
 }
