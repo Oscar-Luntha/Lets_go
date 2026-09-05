70339cd76756b40a7f9fde403790675be7d0af56
Oscar Luntha <lunthaoscar@gmail.com>
2026-09-05 21:54:09 +0200

    Implement user signup form handling with validation checks

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -102,6 +102,23 @@ func (app *application) getUserSignup(w http.ResponseWriter, r *http.Request) {
 	app.render(w, r, http.StatusOK, "signup.tmpl.html", data)
 }
 func (app *application) postUserSignup(w http.ResponseWriter, r *http.Request) {
+	var form userSignUpForm
+	err := app.decodePostForm(r, &form)
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+	form.CheckField(validator.NotBlank(form.Name), "name", "This field cannot be blank")
+	form.CheckField(validator.NotBlank(form.Email), "email", "This field cannot be blank")
+	form.CheckField(validator.Matches(form.Email, validator.EmailRX), "email", "This field must be a valid email address")
+	form.CheckField(validator.NotBlank(form.Password), "password", "This field cannot be blank")
+	form.CheckField(validator.MinChars(form.Password, 8), "password", "This field must be at least 8 characters long")
+	if !form.Valid() {
+		data := app.newTemplateData(r)
+		data.Form = form
+		app.render(w, r, http.StatusUnprocessableEntity, "signup.tmpl.html", data)
+		return
+	}
 	fmt.Fprintln(w, "Create a new user...")
 }
 func (app *application) getUserLogin(w http.ResponseWriter, r *http.Request) {
