6c33054d49824ca1961cb609fb121ffc527a2042
Oscar Luntha <lunthaoscar@gmail.com>
2026-09-08 09:21:45 +0200

    reorganize routes to group protected and unprotected application routes

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -11,18 +11,20 @@ func (app *application) routes() http.Handler {
 
 	fileServer := http.FileServer(http.Dir("./ui/static"))
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-
+	// Unprotected application routes using the "dynamic" middleware chain
 	dynamic := alice.New(app.sessionManager.LoadAndSave)
+
 	mux.Handle("GET /{$}", dynamic.ThenFunc(app.home))
 	mux.Handle("GET /snippet/view/{id}", dynamic.ThenFunc(app.snippetView))
-	mux.Handle("GET /snippet/create", dynamic.ThenFunc(app.getSnippetCreate))
-	mux.Handle("POST /snippet/create", dynamic.ThenFunc(app.postSnippetCreate))
-
 	mux.Handle("GET /user/signup", dynamic.ThenFunc(app.getUserSignup))
 	mux.Handle("POST /user/signup", dynamic.ThenFunc(app.postUserSignup))
 	mux.Handle("GET /user/login", dynamic.ThenFunc(app.getUserLogin))
 	mux.Handle("POST /user/login", dynamic.ThenFunc(app.postUserLogin))
-	mux.Handle("POST /user/logout", dynamic.ThenFunc(app.postUserLogout))
+
+	protected := dynamic.Append(app.requireAuthentication)
+	mux.Handle("GET /snippet/create", protected.ThenFunc(app.getSnippetCreate))
+	mux.Handle("POST /snippet/create", protected.ThenFunc(app.postSnippetCreate))
+	mux.Handle("POST /user/logout", protected.ThenFunc(app.postUserLogout))
 	standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
 	return standard.Then(mux)
 
